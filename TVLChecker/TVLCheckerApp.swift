ec355d1190d936a3c519d7df0c425d621fea8004
Riley Usagi <riley.usagi@gmail.com>
2021-10-26 20:15:50 +0300

    Init

---
+++ TVLChecker/TVLCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVLCheckerApp.swift
+//  TVLChecker
+//
+//  Created by Riley Usagi on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TVLCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
