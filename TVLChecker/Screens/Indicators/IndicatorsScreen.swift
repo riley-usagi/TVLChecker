2d471a8f0be8b4ae1ab4251f33ce399dff4292a0
Riley Usagi <riley.usagi@gmail.com>
2021-10-28 09:11:47 +0300

    Finished

--- TVLChecker/Screens/Indicators/IndicatorsScreen.swift
+++ TVLChecker/Screens/Indicators/IndicatorsScreen.swift
@@ -2,11 +2,14 @@ import SwiftUI
 
 struct IndicatorsScreen: View {
   
+  @Environment(\.colorScheme) var colorScheme
+  
   @State private var searchQuery: String = ""
   
-  @State private var offset: CGFloat      = 0
-  @State private var startOffset: CGFloat = 0
-  @State private var titleOffset: CGFloat = 0
+  @State private var offset: CGFloat          = 0
+  @State private var startOffset: CGFloat     = 0
+  @State private var titleOffset: CGFloat     = 0
+  @State private var titleBarHeight: CGFloat  = 0
   
   var items: [[String: String]] = [
     ["name": "Нефтепродукты", "detail": "Питьевая воды"]
@@ -14,94 +17,132 @@ struct IndicatorsScreen: View {
   
   var body: some View {
     
-    VStack {
+    ZStack(alignment: .top) {
+      
       VStack {
         
-        HStack {
+        if searchQuery == "" {
           
-//          Button {
-//
-//          } label: {
-//            Image(systemName: "star")
-//              .font(.title2)
-//              .foregroundColor(.primary)
-//          }
-          
-          Spacer()
-          
-          Button {
+          HStack {
             
-          } label: {
-            Image(systemName: "questionmark.circle")
-              .font(.title2)
-              .foregroundColor(.primary)
+            Spacer()
+            
+            Button {
+              
+            } label: {
+              Image(systemName: "questionmark.circle")
+                .font(.title2)
+                .foregroundColor(.primary)
+            }
           }
-        }
-        .padding()
-        
-        HStack {
+          .padding()
           
-          Group {
-            Text("Список ")
-              .fontWeight(.bold)
+          HStack {
             
-            +
+            Group {
+              Text("Список ")
+                .fontWeight(.bold)
+              
+              +
+              
+              Text("показателей")
+                .foregroundColor(.gray)
+              
+            }
+            .font(.largeTitle)
+            .overlay(
+              
+              GeometryReader { reader -> Color in
+                
+                let width = reader.frame(in: .global).maxX
+                
+                DispatchQueue.main.async {
+                  
+                  if titleOffset == 0 {
+                    titleOffset = width
+                  }
+                }
+                
+                return .clear
+              }.frame(width: 0, height: 0)
+            )
+            .padding()
+            .scaleEffect(getScale())
+            .offset(getOffset())
             
-            Text("показателей")
+            Spacer()
+          }
+          
+        }
+        
+        VStack {
+          
+          HStack(spacing: 15) {
+            
+            Image(systemName: "magnifyingglass")
+              .font(.system(size: 23, weight: .bold))
               .foregroundColor(.gray)
             
+            TextField("Поиск", text: $searchQuery)
           }
-          .font(.largeTitle)
-          .overlay(
+          .padding(.vertical, 10)
+          .padding(.horizontal)
+          .background(Color.primary.opacity(0.05))
+          .cornerRadius(8)
+          .padding(.horizontal)
+          
+          if searchQuery == "" {
             
-            GeometryReader { reader -> Color in
+            HStack {
               
-              let width = reader.frame(in: .global).maxX
+              Text("НЕДАВНИЕ")
+                .font(.caption)
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
               
-              DispatchQueue.main.async {
-                
-                if titleOffset == 0 {
-                  titleOffset = width
-                }
-              }
-              
-              return .clear
-            }.frame(width: 0, height: 0)
-          )
-          .padding()
-          .offset(getOffset())
+              Rectangle()
+                .fill(Color.gray.opacity(0.6))
+                .frame(height: 0.5)
+            }
+            .padding()
+            
+          }
           
-          Spacer()
         }
-        
-        
-        HStack(spacing: 15) {
-          
-          Image(systemName: "magnifyingglass")
-            .font(.system(size: 23, weight: .bold))
-            .foregroundColor(.gray)
-          
-          TextField("Поиск", text: $searchQuery)
-        }
-        .padding(.vertical, 10)
-        .padding(.horizontal)
-        .background(Color.primary.opacity(0.05))
-        .cornerRadius(8)
-        .padding(.horizontal)
-        
-        HStack {
-          
-          Text("НЕДАВНИЕ")
-            .font(.caption)
-            .fontWeight(.semibold)
-            .foregroundColor(.gray)
-          
-          Rectangle()
-            .fill(Color.gray.opacity(0.6))
-            .frame(height: 0.5)
-        }
-        .padding()
+        .offset(y: offset > 0 && searchQuery == "" ? (offset <= 95 ? -offset : -95) : 0)
       }
+      .zIndex(1)
+      .padding(.bottom, searchQuery == "" ? getOffset().height : 0)
+      .background(
+        
+        ZStack {
+          
+          let color = colorScheme == .dark ? Color.black : Color.white
+          
+          LinearGradient(
+            gradient: .init(colors: [color, color, color, color.opacity(0.6)]),
+            startPoint: .top,
+            endPoint: .bottom
+          )
+        }.ignoresSafeArea()
+        
+      )
+      .overlay(
+        
+        GeometryReader { reader -> Color in
+          
+          let height = reader.frame(in: .global).maxY
+          
+          DispatchQueue.main.async {
+            if titleBarHeight == 0 {
+              titleBarHeight = height - (UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0)
+            }
+          }
+          
+          return .clear
+        }
+      )
+      .animation(.easeInOut, value: searchQuery != "")
       
       ScrollView(.vertical, showsIndicators: false) {
         
@@ -112,6 +153,7 @@ struct IndicatorsScreen: View {
           }
         }
         .padding(.top, 10)
+        .padding(.top, searchQuery == "" ? titleBarHeight : 90)
         .overlay(
           
           GeometryReader { reader -> Color in
@@ -125,8 +167,6 @@ struct IndicatorsScreen: View {
               }
               
               offset = startOffset - minY
-              
-              print(offset)
             }
             
             return .clear
@@ -146,5 +186,17 @@ struct IndicatorsScreen: View {
     size.height = offset > 0 ? (offset <= 75 ? -offset : -75) : 0
     
     return size
+  }
+  
+  private func getScale() -> CGFloat {
+    if offset > 0 {
+      let screenWidth = UIScreen.main.bounds.width
+      
+      let progress = 1 - (getOffset().width / screenWidth)
+      
+      return progress >= 0.7 ? progress: 0.7
+    } else {
+      return 1
+    }
   }
 }
